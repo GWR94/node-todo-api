d18701e024b232c129b701ceefe4b58d96048254
James <james_gower@outlook.com>
2018-04-24 18:20:06 +0100

    added GET /todos route

--- server/server.js
+++ server/server.js
@@ -18,6 +18,17 @@ app.post('/todos', (req, res) => {
 	todo.save().then(doc => res.send(doc), e => res.status(400).send(e));
 });
 
+app.get('/todos', (req, res) => {
+	Todo.find().then(
+		todos => {
+			res.send({ todos });
+		},
+		e => {
+			res.status(400).send(e);
+		}
+	);
+});
+
 app.listen(port, () => {
 	console.log(`Server is up on port ${port}`);
 });
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -11,35 +11,36 @@ beforeEach(done => {
 describe('POST /todos', () => {
 	it('should create a new todo', done => {
 		var text = 'Test todo text';
-		request(app)
+		request(app) //supertest request
 			.post('/todos')
 			.send({ text })
-			.expect(200)
+			.expect(200) //should have a status code of 200 if all went well
 			.expect(res => {
-				expect(res.body.text).toBe(text);
+				expect(res.body.text).toBe(text); //the body of the response should be the same as the variable set earlier
 			})
 			.end((err, res) => {
 				if (err) {
-					return done(err);
+					return done(err); //if there are any errors, return the error and call done()
 				}
 				Todo.find()
 					.then(todos => {
-						expect(todos.length).toBe(1);
-						expect(todos[0].text).toBe(text);
-						done();
+						expect(todos.length).toBe(1); //there should only be 1 documents as the database is cleared before every test
+						expect(todos[0].text).toBe(text); //expect the text of the only document to be the same as the variable set earlier.
+						done(); //finish the test
 					})
-					.catch(e => done(e));
+					.catch(e => done(e)); //if theres any errors, finish the test with the error as a paramater
 			});
 	});
 	it('should not create todo with invalid body data', done => {
 		request(app)
 			.post('/todos')
-			.expect(400)
+			.send({}) //send empty body, should fail the test
+			.expect(400) //should be a 400 status code - bad request
 			.end((err, res) => {
 				if (err) return done(err);
 				Todo.find()
 					.then(todos => {
-						expect(todos.length).toBe(0);
+						expect(todos.length).toBe(0); //should be zero as it shouldnt have saved if there is invalid data
 						done();
 					})
 					.catch(e => done(e));
