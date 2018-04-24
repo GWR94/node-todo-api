215493e75c279bd6809d3810c1e90c436e6521f3
James <james_gower@outlook.com>
2018-04-24 18:37:53 +0100

    added tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -12,19 +12,19 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-	var todo = new Todo({
+	var todo = new Todo({ //Create a new document for the Todo collection with the req.body.text from the request
 		text: req.body.text,
 	});
-	todo.save().then(doc => res.send(doc), e => res.status(400).send(e));
-});
+	todo.save().then(doc => res.send(doc), e => res.status(400).send(e)); //If all goes well, save it and send it.
+}); //if theres an error, send that instead and set 400 code.
 
 app.get('/todos', (req, res) => {
-	Todo.find().then(
+	Todo.find().then( //find everything in the Todos collection
 		todos => {
-			res.send({ todos });
+			res.send({ todos }); //sending the todos array with destructuring allows the option for adding more to send() in the future
 		},
 		e => {
-			res.status(400).send(e);
+			res.status(400).send(e); //If there's any errors, set a 400 (Bad request) status code, and send the error.
 		}
 	);
 });
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,21 @@ const request = require('supertest');
 const { app } = require('../server');
 const { Todo } = require('../models/todo');
 
+const todos = [
+	{
+		text: 'First test todo',
+	},
+	{
+		text: 'Second test todo',
+	},
+];
+
 beforeEach(done => {
-	Todo.remove({}).then(() => done());
+	Todo.remove({})
+		.then(() => {
+			Todo.insertMany(todos);
+		})
+		.then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -22,15 +35,16 @@ describe('POST /todos', () => {
 				if (err) {
 					return done(err); //if there are any errors, return the error and call done()
 				}
-				Todo.find()
+				Todo.find({ text })
 					.then(todos => {
 						expect(todos.length).toBe(1); //there should only be 1 documents as the database is cleared before every test
 						expect(todos[0].text).toBe(text); //expect the text of the only document to be the same as the variable set earlier.
 						done(); //finish the test
 					})
-					.catch(e => done(e)); //if theres any errors, finish the test with the error as a paramater
+					.catch(e => done(e)); //if theres any errors, finish the test with the error as a parameter
 			});
 	});
+
 	it('should not create todo with invalid body data', done => {
 		request(app)
 			.post('/todos')
@@ -40,10 +54,20 @@ describe('POST /todos', () => {
 				if (err) return done(err);
 				Todo.find()
 					.then(todos => {
-						expect(todos.length).toBe(0); //should be zero as it shouldnt have saved if there is invalid data
+						expect(todos.length).toBe(2); //should be two as the new document shouldnt have saved if there is invalid data
 						done();
 					})
 					.catch(e => done(e));
 			});
+	});
+
+	it('should get all todos', (done) => {
+		request(app)
+			.get('/todos')
+			.expect(200)
+			.expect(res => {
+				expect(res.body.todos.length).toBe(2);
+			})
+			.end(done);
 	});
 });
