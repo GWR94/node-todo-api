f845c2511b65e86efe9beb816cc7c9216772f2ac
James <james_gower@outlook.com>
2018-04-26 11:45:24 +0100

    added GET /todos/:id request

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -12,14 +13,26 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-	var todo = new Todo({ //Create a new document for the Todo collection with the req.body.text from the request
+	var todo = new Todo({
+		//Create a new document for the Todo collection with the req.body.text from the request
 		text: req.body.text,
 	});
 	todo.save().then(doc => res.send(doc), e => res.status(400).send(e)); //If all goes well, save it and send it.
 }); //if theres an error, send that instead and set 400 code.
 
+app.get('/todos/:id', (req, res) => {
+	const id = req.params.id;
+	if (!ObjectID.isValid(id)) return res.status(404).send({}); //if the req.params.id isn't a valid objectID, return a 404 error.
+	Todo.findById(id).then(
+		todo => {
+			if(!todo) return res.status(400).send({}); //if there is no todo, return a 400 bad request status code and empty body
+			res.send({ todo }); // else send the todo
+		}).catch(e => res.status(404).send({}))
+});
+
 app.get('/todos', (req, res) => {
-	Todo.find().then( //find everything in the Todos collection
+	Todo.find().then(
+		//find everything in the Todos collection
 		todos => {
 			res.send({ todos }); //sending the todos array with destructuring allows the option for adding more to send() in the future
 		},
