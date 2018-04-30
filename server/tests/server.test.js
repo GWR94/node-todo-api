b69786fe2e9820dceee32b40fd65557c8d953d58
James <james_gower@outlook.com>
2018-04-30 09:20:35 +0100

    added eslint config and tests for DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -34,7 +34,7 @@ describe('POST /todos', () => {
 			.expect(res => {
 				expect(res.body.text).toBe(text); //the body of the response should be the same as the variable set earlier
 			})
-			.end((err, res) => {
+			.end(err => {
 				if (err) {
 					return done(err); //if there are any errors, return the error and call done()
 				}
@@ -53,7 +53,7 @@ describe('POST /todos', () => {
 			.post('/todos')
 			.send({}) //send empty body, should fail the test
 			.expect(400) //should be a 400 status code - bad request
-			.end((err, res) => {
+			.end(err => {
 				if (err) return done(err);
 				Todo.find()
 					.then(todos => {
@@ -88,7 +88,7 @@ describe('GET /todos/:id', () => {
 
 	it('should return an 404 error if the objectID is invalid', done => {
 		request(app)
-			.get(`/todos/123`)
+			.get('/todos/123')
 			.expect(404)
 			.end(done);
 	});
@@ -96,6 +96,44 @@ describe('GET /todos/:id', () => {
 	it('should return a 404 if the todo is not in the collection, but is a valid ObjectID', done => {
 		request(app)
 			.get(`/todos/${new ObjectID().toHexString()}`)
+			.expect(404)
+			.end(done);
+	});
+});
+
+describe('DELETE /todos/:id', () => {
+	it('should delete a document from the todos collection by id', done => {
+		const hexId = todos[0]._id.toHexString();
+		request(app)
+			.delete(`/todos/${hexId}`)
+			.expect(200)
+			.expect(res => {
+				expect(res.body.todo._id).toBe(hexId);
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+				Todo.findById(hexId)
+					.then(todo => {
+						expect(todo).toBeFalsy();
+						done();
+					})
+					.catch(e => done(e));
+			});
+	});
+
+	it('should return 404 if the ObjectID is invalid', done => {
+		request(app)
+			.delete(`/todos/abc123`)
+			.expect(404)
+			.end(done);
+	});
+
+	it('should return 404 if the document doesnt exist', done => {
+		let hexID = new ObjectID().toHexString();
+		request(app)
+			.delete(`/todos/${hexID}`)
 			.expect(404)
 			.end(done);
 	});
