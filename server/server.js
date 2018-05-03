5bb261fb03d5f3eb9c186744cd969e300130e5d9
James <james_gower@outlook.com>
2018-05-03 17:17:19 +0100

    updated server.js to use async/await syntax

--- server/server.js
+++ server/server.js
@@ -17,53 +17,55 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 
 //: POST ://
-app.post('/todos', authenticate, (req, res) => {
+app.post('/todos', authenticate, async (req, res) => {
 	var todo = new Todo({
 		//Create a new document for the Todo collection with the req.body.text from the request
 		text: req.body.text,
 		_creator: req.user._id,
 	});
-	todo.save().then(todo => res.send(todo), e => res.status(400).send(e)); //If all goes well, save it and send it.
-}); //if theres an error, send that instead and set 400 code.
-
-app.post('/users', (req, res) => {
-	const body = _.pick(req.body, ['email', 'password']);
-	const user = new User(body);
-	user
-		.save()
-		.then(() => {
-			return user.generateAuthToken(); //creates auth token from UserSchema in user.js
-		})
-		.then(token => {
-			res.header('x-auth', token).send(user); //x-auth sets custom header and puts token inside it, then sends the user
-		})
-		.catch(e => {
-			return res.status(400).send(e);
-		});
+	try {
+		await todo.save(); //If all goes well, save it and send it.
+		await res.send(todo);
+	} catch (e) {
+		res.status(400).send(e); //if theres an error, send that instead and set 400 code.
+	}
 });
 
-app.post('/users/login', (req, res) => {
-	const { email, password } = _.pick(req.body, ['email', 'password']);
-	User.findByCredentials(email, password)
-		.then(user => {
-			return user.generateAuthToken().then(token => {
-				//return so if theres an error it will be caught in the catch block
-				res.header('x-auth', token).send(user);
-			});
-		})
-		.catch(e => res.status(400).send(e));
+app.post('/users', async (req, res) => {
+	const body = _.pick(req.body, ['email', 'password']);
+	const user = new User(body);
+	try {
+		await user.save();
+		const token = await user.generateAuthToken(); //creates auth token from UserSchema in user.js
+		res.header('x-auth', token).send(user); //x-auth sets custom header and puts token inside it, then sends the user
+	} catch (e) {
+		res.status(400).send(e);
+	}
+});
+
+app.post('/users/login', async (req, res) => {
+	try {
+		const { email, password } = _.pick(req.body, ['email', 'password']);
+		const user = await User.findByCredentials(email, password);
+		const token = await user.generateAuthToken();
+		//return so if theres an error it will be caught in the catch block
+		res.header('x-auth', token).send(user);
+	} catch (e) {
+		res.status(400).send(e);
+	}
 });
 
 //: GET ://
-app.get('/todos/:id', authenticate, (req, res) => {
+app.get('/todos/:id', authenticate, async (req, res) => {
 	const id = req.params.id;
 	if (!ObjectID.isValid(id)) return res.status(404).send(); //if the req.params.id isn't a valid objectID, return a 404 error.
-	Todo.findOne({ _id: id, _creator: req.user._id })
-		.then(todo => {
-			if (!todo) return res.status(404).send(); //if there is no todo, return a 404 status code and empty body
-			res.send({ todo }); // else send the todo
-		})
-		.catch(err => res.status(400).send(err));
+	try {
+		const todo = await Todo.findOne({ _id: id, _creator: req.user._id });
+		if (!todo) return res.status(404).send(); //if there is no todo, return a 404 status code and empty body
+		res.send({ todo }); // else send the todo
+	} catch (e) {
+		res.status(400).send(e);
+	}
 });
 
 app.get('/users/me', authenticate, (req, res) => {
@@ -71,41 +73,40 @@ app.get('/users/me', authenticate, (req, res) => {
 	res.send(req.user);
 });
 
-app.get('/todos', authenticate, (req, res) => {
-	Todo.find({ _creator: req.user._id }).then(
+app.get('/todos', authenticate, async (req, res) => {
+	try {
 		//find everything in the Todos collection
-		todos => res.send({ todos }), //sending the todos array with destructuring allows the option for adding more to send() in the future
-		err => res.status(400).send(err) //If there's any errors, set a 400 (Bad request) status code, and send the error.
-	);
+		const todos = await Todo.find({ _creator: req.user._id });
+		res.send({ todos }); //sending the todos array with destructuring allows the option for adding more to send() in the future
+	} catch (e) {
+		res.status(400).send(e); //If there's any errors, set a 400 (Bad request) status code, and send the error.
+	}
 });
 
 //: DELETE ://
-app.delete('/todos/:id', authenticate, (req, res) => {
+app.delete('/todos/:id', authenticate, async (req, res) => {
 	const id = req.params.id;
 	if (!ObjectID.isValid(id)) return res.status(404).send(); //if invalid ObjectID, return a 404 error as nothing can be deleted
-	Todo.findOneAndRemove({ _id: id, _creator: req.user._id })
-		.then(todo => {
-			if (!todo) {
-				return res.status(404).send(); //if the document doesn't exist, return a 404 status code and empty body
-			}
-			res.send({ todo }); //If there is no problems, delete it and send the document data
-		})
-		.catch(err => res.status(400).send(err));
+	try {
+		const todo = await Todo.findOneAndRemove({ _id: id, _creator: req.user._id });
+		if (!todo) return res.status(404).send(); //if the document doesn't exist, return a 404 status code and empty body
+		res.send({ todo }); //If there is no problems, delete it and send the document data
+	} catch (e) {
+		res.status(400).send(e);
+	}
 });
 
-app.delete('/users/me/token', authenticate, (req, res) => {
-	req.user.removeToken(req.token).then(
-		() => {
-			res.send();
-		},
-		err => {
-			res.status(400).send(err);
-		}
-	);
+app.delete('/users/me/token', authenticate, async (req, res) => {
+	try {
+		await req.user.removeToken(req.token);
+		res.send();
+	} catch (e) {
+		res.status(400).send(e);
+	}
 });
 
 //: PATCH ://
-app.patch('/todos/:id', authenticate, (req, res) => {
+app.patch('/todos/:id', authenticate, async (req, res) => {
 	const id = req.params.id;
 	const body = _.pick(req.body, ['text', 'completed']); //_.pick check the body for the values in the array. Doesn't return any values which are not in the array.
 	if (!ObjectID.isValid(id)) return res.status(404).send(); //if the req.params.id isn't a valid objectID, return a 404 error.
@@ -119,14 +120,13 @@ app.patch('/todos/:id', authenticate, (req, res) => {
 		body.completedAt = null;
 	}
 
-	Todo.findOneAndUpdate({_id: id, _creator: req.user._id}, { $set: body }, { new: true }) //new: false returns the updated document and not the original, $set sets the body on the new document.
-		.then(todo => {
-			if (!todo) {
-				return res.status(404).send(); //if no todo, return a 404 error
-			}
-			res.send({ todo }); //else send the updated todo
-		})
-		.catch(e => res.status(400).send(e)); //if any other errors return a 400 status code
+	const todo = await Todo.findOneAndUpdate({ _id: id, _creator: req.user._id }, { $set: body }, { new: true }); //new: false returns the updated document and not the original, $set sets the body on the new document.
+	try {
+		if (!todo) return res.status(404).send(); //if no todo, return a 404 error
+		res.send({ todo }); //else send the updated todo
+	} catch (e) {
+		res.status(400).send(e); //if any other errors return a 400 status code
+	}
 });
 
 //: LISTEN ://
